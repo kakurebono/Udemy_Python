c4ce6ce325496d337b914538d7128220470d768b
kakurebono <55621277+kakurebono@users.noreply.github.com>
2020-12-04 14:23:51 +0100

    playing around

--- Udemy_Python_beginner_exercises.py
+++ Udemy_Python_beginner_exercises.py
@@ -176,3 +176,30 @@ print("The Fahrenheit equivalent of " + str(cTemp) + " degrees Celsius is " + st
 #
 
 '''
+
+#string methods 2 exercises
+
+
+#Create a variable called the_string and assign it the string "North Dakota".
+
+the_string = "North Dakota"
+
+#Call .rjust() on the_string with 17 as its argument and print() the result.
+
+print(the_string.strip(17))
+
+
+
+# Call .ljust() on the_string with the arguments 17 and "*" then print() the result.
+
+# Create a variable called center_plus and assign it the result of .center() being called on the_string with 16 and "+" as arguments.
+
+# Use print() to display the string assigned to center_plus.
+
+# Call .lstrip() on the_string to remove "North" then print() the result.
+
+# Call .rstrip() on center_plus with "+" as its argument and print() the result.
+
+# Call .strip() on center_plus with "+" as its argument and print() the result.
+
+# Call .replace() on the_string and replace "North" with "South".  print() the result
